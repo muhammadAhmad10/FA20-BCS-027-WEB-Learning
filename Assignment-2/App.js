fe32a5b63150258726dc40e21ceef38959a79bff
Muhammad Ahmad <ahmadcs442@gmail.com>
2023-03-22 21:57:28 +0500

    added new pages

---
+++ Assignment-2/App.js
@@ -0,0 +1,8 @@
+// $(function(){
+//     $("#good").on("click","li",function(){
+//         $(this).appendTo("#baf")
+//     });
+//     $("#bad").on("click","li",function(){
+//         $(this).appendTo("#good")
+//     });
+// })
--- Login/Login.js
+++ Login/Login.js
@@ -1,15 +1,21 @@
 
-function passwordFocus(){
+function handleSubmit(){
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     var input = document.getElementById("floatingInput");
     var input1 = document.getElementById('floatingPassword')
-    if (input.value) {
+    if (input.value && input.value.match(validRegex)) {
+        console.log('Email is: ',input.value)
+        input.value = ""
         input.classList.remove("error");
         input.classList.add("success");
       } else {
         input.classList.remove("success");
         input.classList.add("error");
       }
-      if (input1.value) {
+      
+      if (input1.value && input1.value.length>=6) {
+        console.log('Password is: ',input1.value)
+        input1.value = ""
         input1.classList.remove("error");
         input1.classList.add("success");
       } else {
